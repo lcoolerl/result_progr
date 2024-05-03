ce3005524f42737e7ee72b9b56c8f674bf38102d
Serg Danilchuk <serg_danilchuk@mail.ru>
2024-05-03 15:50:47 +0800

    add 3 task

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Программирование Итоговое 2
+
+// Ввод чисел
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+// Первый вызов функции
+if (m >= 0 && n >= 0)
+{
+   Console.Write(Akkerm(m, n));
+}
+
+// Функция
+int Akkerm(int a, int b)
+{
+    if (a == 0) return b + 1;
+    if (a > 0 && b == 0) return Akkerm(a - 1, 1);
+    else return Akkerm(a-1, Akkerm(a,b-1));
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Программирование Итоговое 3
+
+// Произвольный массив
+char[] array = new char [] {'a','g','7','G','l'};
+int count=array.Length-1;
+
+PrntReverse (array, count);
+
+void PrntReverse (char[] STR, int a)
+{
+    if (a>=0) 
+    {
+    Console.Write($"{STR[a]} ");
+    PrntReverse (array, --a);
+    }
+}
